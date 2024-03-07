e0a419153485f49d6f744c3b6936b56019d86ceb
weesleycabral <weesleylima@hotmail.com>
2024-03-07 15:53:16 -0300

    Update Firebase configuration files

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/firebase_options.dart';
 import 'package:flutter_chat/services/auth/auth_gate.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  // if (Platform.isIOS) {
-  //   await Firebase.initializeApp(
-  //       options: const FirebaseOptions(
-  //           apiKey: "AIzaSyBNgxR8pqRMpK8ndEqZCVpDtE05EulvclQ",
-  //           appId: "app-1-89099581573-ios-a2068199649cb3838691ec",
-  //           messagingSenderId: "89099581573",
-  //           projectId: "1:89099581573:ios:a2068199649cb3838691ec"));
-  // } else {
-  //   await Firebase.initializeApp();
-  // }
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
